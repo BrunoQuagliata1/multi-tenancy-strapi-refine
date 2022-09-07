0987f701b33d005989721148010ce6c5ae6eb44b
Bruno Quagliata <pcerdadelpaco@192.168.1.3>
2022-09-07 18:52:31 -0300

    feat: multi-tenancy project first commit

---
+++ src/components/sider/index.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import {
+  useTitle,
+  useNavigation,
+  useLogout,
+  ITreeMenu,
+  CanAccess
+} from "@pankod/refine-core";
+import { AntdLayout, Menu, useMenu, Grid, Icons } from "@pankod/refine-antd";
+import { antLayoutSider, antLayoutSiderMobile } from "./styles";
+
+import { StoreSelect } from "components/select";
+
+export const CustomSider: React.FC = () => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  const { mutate: logout } = useLogout();
+  const Title = useTitle();
+  const { SubMenu } = Menu;
+  const { menuItems, selectedKey } = useMenu();
+  const breakpoint = Grid.useBreakpoint();
+  const { push } = useNavigation();
+
+  const isMobile = !breakpoint.lg;
+
+  const renderTreeView = (tree: ITreeMenu[], selectedKey: string) => {
+    return tree.map((item: ITreeMenu) => {
+      const { icon, label, route, name, children, parentName } = item;
+
+      if (children.length > 0) {
+        return (
+          <SubMenu
+            key={name}
+            icon={icon ?? <Icons.UnorderedListOutlined />}
+            title={label}
+          >
+            {renderTreeView(children, selectedKey)}
+          </SubMenu>
+        );
+      }
+      const isSelected = route === selectedKey;
+      const isRoute = !(parentName !== undefined && children.length === 0);
+      return (
+        <CanAccess key={route} resource={name.toLowerCase()} action="list">
+          <Menu.Item
+            key={selectedKey}
+            onClick={() => {
+              push(route ?? "");
+            }}
+            style={{
+              fontWeight: isSelected ? "bold" : "normal"
+            }}
+            icon={icon ?? (isRoute && <Icons.UnorderedListOutlined />)}
+          >
+            {label}
+            {!collapsed && isSelected && (
+              <div className="ant-menu-tree-arrow" />
+            )}
+          </Menu.Item>
+        </CanAccess>
+      );
+    });
+  };
+  return (
+    <AntdLayout.Sider
+      collapsible
+      collapsedWidth={isMobile ? 0 : 80}
+      collapsed={collapsed}
+      breakpoint="lg"
+      onCollapse={(collapsed: boolean): void => setCollapsed(collapsed)}
+      style={isMobile ? antLayoutSiderMobile : antLayoutSider}
+    >
+      {Title && <Title collapsed={collapsed} />}
+      <Menu
+        selectedKeys={[selectedKey]}
+        mode="inline"
+        onClick={({ key }) => {
+          if (key === "logout") {
+            logout();
+            return;
+          }
+
+          push(key as string);
+        }}
+      >
+        <Menu.Item key={selectedKey} icon={<Icons.AppstoreAddOutlined />}>
+          <StoreSelect
+            onSelect={() => {
+              setCollapsed(true);
+            }}
+          />
+        </Menu.Item>
+        {renderTreeView(menuItems, selectedKey)}
+        <Menu.Item key={"logout"} icon={<Icons.LoginOutlined />}>
+          Logout
+        </Menu.Item>
+      </Menu>
+    </AntdLayout.Sider>
+  );
+};
